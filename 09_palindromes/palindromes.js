02b953bbbdb94826340298fca05c40c1d5e897ed
misterbanhman <nathan.i.banh@gmail.com>
2022-10-23 02:45:21 -0600

    Add removeSpecialCharacter, arrayToLowerCase, and toArray functions. Pass all 6 tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,12 @@
 const palindromes = function (string) {
-    let stringToArray = string.split('');
+    let stringToArray = toArray(string);
+    stringToArray = removeSpecialCharacter(stringToArray);
+    stringToArray = arrayToLowerCase(stringToArray);
 
     let halfIndex = Math.floor(stringToArray.length/2);
     let endIndex = stringToArray.length-1;
     let palindromesCheck = true;
 
-    if (stringToArray.length % 2 != 0){
         for (let i=0;i<halfIndex;i++) {
             if (stringToArray[i] === stringToArray[endIndex-i]) {
                 palindromesCheck = true;
@@ -16,12 +17,41 @@ const palindromes = function (string) {
             }
         }
         return true;
-    }
-
-    else {
-        return false;
-    }
 };
+
+function toArray(string) {
+    let array = string.split('');
+    return array;
+}
+
+function removeSpecialCharacter(array) {
+    let toRemove = ['!',',','.',' '];
+    
+    let lengthOfArray = array.length;
+    let lengthOfRemovedItems = toRemove.length;
+
+    let indexOfElement
+
+    for (let i=0;i<lengthOfArray;i++) {
+        for (let j=0;j<lengthOfRemovedItems;j++) {
+            indexOfElement = array.indexOf(toRemove[j]);
+            if (indexOfElement !== -1) {
+                array.splice(indexOfElement,1);
+            }
+        }
+    }
+    return array;   
+}
+
+function arrayToLowerCase(array) {
+    let modifiedArray = '';
+    let lengthOfArray = array.length;
+
+    for (let i=0;i<lengthOfArray;i++) {
+        modifiedArray += array[i].toLowerCase();
+    }
+    return modifiedArray;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
