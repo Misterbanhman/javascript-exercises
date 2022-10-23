eb6b36c4e1a939e9ec8316a6e9836b10a9c537e5
misterbanhman <nathan.i.banh@gmail.com>
2022-10-23 15:54:32 -0600

    Finish exercise 10_fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number === 1 || number === 2) {
+        return 1;
+    }
+
+    else if (number<0) {
+        return "OOPS"
+    }
+
+    else {
+        let currentValue = 2;
+        let previousValue = 1;
+        let nextValue;
+
+        for (let i=2;i<number;i++) {
+            nextValue = currentValue + previousValue;
+            previousValue = currentValue;
+            currentValue = nextValue;
+        }
+        return previousValue;
+    }
 
 };
 
